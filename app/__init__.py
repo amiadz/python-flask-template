8c4231fe7b12eb0b33f5e7ffb065830c0b960a2d
amiad <makomtovapp@gmail.com>
2022-10-02 15:16:57 +0300

    add app config

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+app.config.from_object('config.LocalConfig')
+
 from app.model import user
 from app import views
 from app import auth_views
--- app/db_util.py
+++ app/db_util.py
@@ -1,5 +1,4 @@
 from app import app
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
