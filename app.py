676bd62d17e24f832d2a898c5e5472883976a69c
amiad <makomtovapp@gmail.com>
2022-10-02 15:16:29 +0300

    remove unes imports

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 from app import app
-from flask import render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
 
-app.config.from_object('config.DevConfig')
-
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 if __name__ == "__main__":
     app.run(debug=True)
