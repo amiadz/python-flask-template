ca1978b14bb530425a279897cc1b54e1e07934b8
amiad <makomtovapp@gmail.com>
2022-10-03 09:59:15 +0300

    change config

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config.from_object('config.LocalConfig')
+app.config.from_object('config.DevConfig')
 
 from app.model import user
 from app import views
